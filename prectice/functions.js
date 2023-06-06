0b5ddcd4b7ec52a7f5bded7a9a7c7677ded16458
Mohd Nadeem <102049855+NadeemFahad2200@users.noreply.github.com>
2023-06-06 18:26:14 +0000

    revice

--- prectice/functions.js
+++ prectice/functions.js
@@ -5,4 +5,14 @@ function greet() {
 }
 
 // calling the function
-greet();
+// greet();
+
+// program to add two numbers using a function
+// declaring a function
+function add(a, b) {
+    console.log(a + b);
+}
+
+// calling functions
+add(3,4);
+add(2,9);

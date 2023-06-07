a5d69a3c96b4864a87801066e450d17298d1fa3c
Mohd Nadeem <102049855+NadeemFahad2200@users.noreply.github.com>
2023-06-07 18:19:41 +0000

    function

--- prectice/function1.js
+++ prectice/function1.js
@@ -1,8 +1,10 @@
-const a = 10;
-const b = 20;
-sum(a,b);
 function sum(a,b){
     const sum = a+b;
     console.log('The result is', sum);
+    return sum;
 
 }
+const a = 10;
+const b = 20;
+sum(a,b);
+

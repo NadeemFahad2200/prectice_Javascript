669e70e96abd73583614bc208e83eec412c4732f
Mohd Nadeem <102049855+NadeemFahad2200@users.noreply.github.com>
2023-06-07 18:35:11 +0000

    Nested function

--- prectice/function1.js
+++ prectice/function1.js
@@ -1,3 +1,4 @@
+// function in javascript
 // function sum(a,b){
 //     const sum = a+b;
 //     console.log('The result is', sum);
@@ -8,10 +9,14 @@
 // const b = 20;
 // sum(a,b);
 
-
+// function expression
 const square = function (num) {
     return num*num;
     
 }
 console.log(square);
 console.log(square(3));
+
+
+// Nested Function 
+

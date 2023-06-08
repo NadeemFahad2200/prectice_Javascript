fba9a6ab312f24c43681c7efc24467b41d642379
Mohd Nadeem <102049855+NadeemFahad2200@users.noreply.github.com>
2023-06-08 12:26:06 +0000

    hiii

--- prectice/f1.js
+++ prectice/f1.js
@@ -3,3 +3,11 @@ function myFunction(g1, g2) {
 }
 const value = myFunction(8, 2); // Calling the function
 console.log(value);
+
+
+
+function nadeem(a ,b){
+    return a + b;
+
+}
+console.log(nadeem(900, 1000));
